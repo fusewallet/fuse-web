a76c3d158ebc2e9a16906cb9e1df05387ec382be
drizzle <drizzlemisty@users.noreply.github.com>
2025-03-11 13:46:36 +0800

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
+import './index.css';
+
+import App from './App';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
+);
---
+++ src/routes.tsx
@@ -0,0 +1,8 @@
+import { RouteObject } from 'react-router';
+
+import HomePage from './pages/Home';
+
+export const routes: RouteObject[] = [
+    { path: '/', element: <HomePage /> },
+    { path: '*', element: <HomePage /> },
+];
